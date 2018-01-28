09a88c297d739e1b017d4f87069cc96f7b7c0c22
Yannick Jamet <yjamet@gmail.com>
2018-01-29 04:33:37 +0700

    LT on space unconfortable, use rctrl and caps lock btn instead

--- keyboards/tada68/keymaps/yannickjmt/keymap.c
+++ keyboards/tada68/keymaps/yannickjmt/keymap.c
@@ -8,7 +8,11 @@
 #define _FL 1
 #define _NL 2
 
-#define SPACE LT(_FL, KC_SPC)
+#define TAPPING_TOGGLE 2 // 2 taps to toggle layer _NL
+
+// #define LCTR LT(_FL, TG(_NL)) doesnt work for some reason
+#define RCTRL LT(_FL, KC_HOME)
+#define CPS LT(_FL, KC_CAPS)
 #define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = KEYMAP_ANSI(
   KC_ESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN, KC_QUOT,         KC_ENT,KC_PGUP,  \
+  CPS   ,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN, KC_QUOT,         KC_ENT,KC_PGUP,  \
   KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,      KC_RSFT,  KC_UP,   KC_PGDN, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,TG(_NL),KC_HOME, KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,TG(_NL), RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -50,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,  KC_UP,_______, _______,_______,_______,KC_MSTP,_______,_______,_______,_______,  _______,KC_HOME, \
   _______,_______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,KC_MRWD,KC_MPLY,KC_MFFD,_______,_______,          _______,KC_END, \
   _______,_______,_______, BL_DEC, BL_TOGG, BL_INC,_______,KC_VOLD,KC_VOLU,KC_MUTE,_______,        KC_BTN1, KC_MS_U, KC_BTN2, \
-  _______,_______,_______,                 _______,                        _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
+  _______,_______,_______,                 _______,                        _______,KC_NO  ,_______,KC_MS_L,KC_MS_D, KC_MS_R),
 
   /* Keymap _NL: Numpad Layer
    * ,----------------------------------------------------------------.
@@ -68,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NL] = KEYMAP_ANSI(
   _______,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_7,   KC_8,   KC_9,KC_KP_SLASH,KC_KP_ASTERISK,KC_KP_EQUAL,KC_BSPC,KC_GRV, \
   _______,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_4,   KC_5,   KC_6,KC_KP_MINUS,       _______, _______,_______,KC_DEL, \
-  _______,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_1,   KC_2,   KC_3, KC_KP_PLUS,       _______,         _______,KC_PGUP,  \
+  KC_NO  ,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_1,   KC_2,   KC_3, KC_KP_PLUS,       _______,         _______,KC_PGUP,  \
   _______,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_0,   KC_0, KC_DOT,    _______,             KC_RSFT,  KC_UP,   KC_PGDN, \
-  _______, _______,_______,                _______,                           _______,_______,_______,      KC_LEFT,KC_DOWN,KC_RGHT),
+  _______, _______,_______,                _______,                           _______,_______,KC_NO   ,      KC_LEFT,KC_DOWN,KC_RGHT),
 };
 
 
