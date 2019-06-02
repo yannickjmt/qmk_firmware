e7a1d0554f86f9e5c08f704b6ba97dfbc4fc7ed8
Yannick Jamet <yjamet@gmail.com>
2019-06-02 12:00:57 +0700

    alt/cmd tab super macro

--- keyboards/xd75/keymaps/yannick/keymap.c
+++ keyboards/xd75/keymaps/yannick/keymap.c
@@ -18,11 +18,18 @@
 
 #include QMK_KEYBOARD_H
 
+// for PREVWIN and NEXTWIN macro
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
+
 // Layer shorthand
-#define _QW 0
-#define _FN 5
-#define _NUM 8
-#define _NAV 6
+#define _MAC 0
+#define _WIN 1
+#define _FN 2
+#define _NUM 3
+#define _NAV 4
+#define _FR 5
 // #define _EM 99
 
 // tap FUNCT : caps lock KC_CAPS, hold : FUNCTION layer
@@ -34,6 +41,7 @@
 #define NUMLAY TT(_NUM)
 // #define NAVLAY LT(_NAV, KC_BSPC)
 #define NAVLAY MO(_NAV)
+#define FRLAY MO(_FR)
 
 // one shot mods for nav layer
 #define N_CTRL OSM(MOD_LGUI) // TODO: MOD_LCTL for windows
@@ -47,16 +55,35 @@
 #define COPY_MC LGUI(KC_C)
 #define PASTE_MC LGUI(KC_V)
 
+// French chars
+enum unicode_names {
+  E_AIG,
+  E_AIG_MAJ,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+  [E_AIG]  = 0x00E9,  // 0 é
+  [E_AIG_MAJ] = 0x00C9,  // 1 É
+};
+
+#define FR_EAIG XP(0,1)
+
+// initialize unicode mode
+void eeconfig_init_user(void) {
+  set_unicode_input_mode(UC_OSX);
+}
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
   QMKBEST = SAFE_RANGE,
-  QMKURL
+  QMKURL,
+  NEXTWIN,
+  PREVWIN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
+/* MAC
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
@@ -70,21 +97,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
-  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
+  [_MAC] = LAYOUT_ortho_5x15( /* QWERTY MAC */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
     FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,SHFT_COL,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_ENT,  NAVLAY,  KC_LEFT, KC_DOWN, KC_RGHT, NUMLAY,  KC_SPC,  XXXXXXX, KC_APP,  KC_RALT, KC_RCTL
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHFT_COL,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_ENT,  NAVLAY,  KC_LEFT, KC_DOWN, KC_RGHT, NUMLAY,  KC_SPC,  FRLAY  , KC_APP,  KC_RALT, KC_RCTL
+  ),
+
+/* WIN
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      | DEL    |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * | FN_CAPS| A      | S      | D      | F      | G      | HOME   | DEL    | PG UP  | H      | J      | K      | L      | '      | ENTER  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+ * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      |SHFT_APO|
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | LGUI   | LCTRL  | LALT   | SPACE  | ENTER  | LOWER  | LEFT   | DOWN   | RIGHT  | RAISE  | SPACE  | FRENCH | APP    | RALT   | RCTRL  |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+  [_WIN] = LAYOUT_ortho_5x15( /* QWERTY WINDOWS */
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+    FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SHFT_COL,
+    KC_LGUI, KC_LCTL, KC_LALT, KC_SPC,  KC_ENT,  NAVLAY,  KC_LEFT, KC_DOWN, KC_RGHT, NUMLAY,  KC_SPC,  FRLAY  , KC_APP,  KC_RALT, KC_RCTL
   ),
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | XXXXXXX| F1     | F2     | F3     | F4     | F5     | F11    | F12    | XXXXXXX| F6     | F7     | F8     | F9     | F10    | RESET  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | XXXXXXX| !      | @      | #      | $      | %      | XXXXXXX| XXXXXXX| XXXXXXX| ^      | &      | *      | (      | )      | XXXXXXX|
+ * | XXXXXXX| !      | @      | #      | $      | %      | XXXXXXX| XXXXXXX| XXXXXXX| ^      | &      | *      | (      | )      | windows|
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| RGB SD | RGB SI | XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX|
+ * | XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| RGB SD | RGB SI | XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| XXXXXXX| mac    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        | XXXXXXX| RGB HD | RGB HI | RGB VD | RGB VI | XXXXXXX|        | XXXXXXX| PR SCR | SCR LK | PAUSE  | XXXXXXX| XXXXXXX|        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -94,8 +143,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
     XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11 , KC_F12 , XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET,
-    XXXXXXX, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, XXXXXXX, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAD, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, XXXXXXX, XXXXXXX, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, DF(_WIN),
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAD, RGB_SAI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DF(_MAC),
     _______, XXXXXXX, RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, XXXXXXX, _______, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS, XXXXXXX, XXXXXXX, _______,
     _______, _______, _______, RGB_TOG, RGB_RMOD,RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
@@ -162,7 +211,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_WH_U, _______,
     _______, N_GUI  , N_SHFT , N_ALT  , N_CTRL , _______, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_WH_D, _______,
-    _______,UNDO_MAC, CUT_MAC, COPY_MC,PASTE_MC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,UNDO_MAC, CUT_MAC, COPY_MC,PASTE_MC, _______, _______, _______, _______, PREVWIN, NEXTWIN, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
+/* FRENCH
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+  [_FR] = LAYOUT_ortho_5x15(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, FR_EAIG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
@@ -186,8 +257,58 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // when keycode QMKURL is released
       }
       break;
+    case NEXTWIN:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          switch (biton32(default_layer_state)) {
+          case _MAC:
+            register_code(KC_LGUI);
+            break;
+          case _WIN:
+            register_code(KC_LALT);
+            break;
+          }
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+    case PREVWIN:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          switch (biton32(default_layer_state)) {
+          case _MAC:
+            register_code(KC_LGUI);
+            break;
+          case _WIN:
+            register_code(KC_LALT);
+            break;
+          }
+        }
+        alt_tab_timer = timer_read();
+        register_code16(S(KC_TAB));
+      } else {
+        unregister_code16(S(KC_TAB));
+      }
+      break;
   }
   return true;
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+  switch (biton32(default_layer_state)) {
+  case _MAC:
+    set_unicode_input_mode(UC_OSX);
+    break;
+  case _WIN:
+    set_unicode_input_mode(UC_WINC);
+    break;
+  }
+  return state;
 }
 
 void matrix_init_user(void) {
@@ -195,7 +316,19 @@ void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
-
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 800) {
+      switch (biton32(default_layer_state)) {
+      case _MAC:
+        unregister_code(KC_LGUI);
+        break;
+      case _WIN:
+        unregister_code(KC_LALT);
+        break;
+      }
+      is_alt_tab_active = false;
+    }
+  }
 }
 
 void led_set_user(uint8_t usb_led) {
--- keyboards/xd75/keymaps/yannick/readme.md
+++ keyboards/xd75/keymaps/yannick/readme.md
@@ -2,26 +2,32 @@
 
 One base layer for the moment (mac)
 [x] hold caps lock => function layer
-[X] tap right shift => '
+[X] tap right shift => ;
 [ ] tap on leftmost buttons to change worspace (ctrl left/right on mac, ctrl+win left/right on windows)
-[ ] add windows version 
+[X] add windows version 
     with inverted left ctrl and lgui keys
 
 function layer :
 [x] F2-F12 on num row
 [x] !@#$%+^&*() on corresponding letter row
 [x] LED control (to develop)
+[x] change default layer
 [ ] email adresses
 [ ] euro sign below dollar
 [ ] thai baht sign ฿
 
-lower layer :
-[ ] numpad on right side
-[ ] brackets on left side
-[ ] double tap toggle
+numpad layer :
+[X] numpad on right side
+[X] brackets on left side
+[X] double tap toggle
 
-raise layer :
-http://www.keyboard-layout-editor.com/##@@_r:10&rx:1&y:-0.1&x:2&a:7%3B&=%3B&@_y:-0.65&x:1%3B&=&_x:1&fa@:7%3B%3B&=%3Ci%20class%2F='kb%20kb-Hamburger-Menu'%3E%3C%2F%2Fi%3E%3B&@_y:-0.75%3B&=%3B&@_y:-0.9&x:4%3B&=%3Ci%20class%2F='fa%20fa-terminal'%3E%3C%2F%2Fi%3E%3B&@_y:-0.7&x:2&a:4&f:3%3B&=Alt%3B&@_y:-0.65&x:1&f:3%3B&=Shift&_x:1&f:3%3B&=Ctrl%3B&@_y:-0.75&a:7%3B&=%3Ci%20class%2F='fa%20fa-windows'%3E%3C%2F%2Fi%3E%3B&@_y:-0.9&x:4%3B&=%3B&@_y:-0.7&x:2%3B&=%3B&@_y:-0.65&x:1%3B&=&_x:1%3B&=%3Ci%20class%2F='fa%20fa-search'%3E%3C%2F%2Fi%3E%3B&@_y:-0.75%3B&=%3B&@_y:-0.9&x:4%3B&=%3Ci%20class%2F='fa%20fa-clipboard'%3E%3C%2F%2Fi%3E%3B&@_y:-0.75&x:5&h:1.5%3B&=%3B&@_y:-0.95&x:2%3B&=%3B&@_y:-0.65&x:1%3B&=&_x:1%3B&=%3B&@_y:-0.75%3B&=%3B&@_y:-0.9&x:4%3B&=%3B&@_r:-10&rx:7&ry:0.965&y:-0.2&x:2%3B&=%3Ci%20class%2F='kb%20kb-Arrows-Up'%3E%3C%2F%2Fi%3E%3B&@_y:-0.65&x:1&a:4&f:3%3B&=Home&_x:1&f:3%3B&=End%3B&@_y:-0.75&x:4&f:3%3B&=Delete%3B&@_y:-0.9&f:3%3B&=PgUp%3B&@_y:-0.7&x:2&a:7%3B&=%3Ci%20class%2F='kb%20kb-Arrows-Down'%3E%3C%2F%2Fi%3E%3B&@_y:-0.65&x:1%3B&=%3Ci%20class%2F='kb%20kb-Arrows-Left'%3E%3C%2F%2Fi%3E&_x:1%3B&=%3Ci%20class%2F='kb%20kb-Arrows-Right'%3E%3C%2F%2Fi%3E%3B&@_y:-0.75&x:4&a:4&f:3%3B&=Bksp%3B&@_y:-0.9&f:3%3B&=PgDn%3B&@_y:-0.7&x:2&f:3%3B&=Tab%3B&@_y:-0.65&x:1&f:3%3B&=Shift+%0A%0A%0A%0A%0A%0ATab&_x:1&a:7%3B&=%3B&@_y:-0.75&x:4&a:4&f:3%3B&=Insert%3B&@_y:-0.9&a:7%3B&=%3B&@_y:-0.75&x:-1&h:1.5%3B&=%3B&@_y:-0.95&x:2%3B&=%3B&@_y:-0.65&x:1%3B&=&_x:1%3B&=%3B&@_y:-0.75&x:4&a:4&f:3%3B&=Prnt%20Scrn%3B&@_y:-0.9&c=%23ffe08d&a:7%3B&=
+nav layer :
+[X] nav on right side
+[X] one tap mod on left side
+    [ ] invert one shot mods depending on base layer
+[x] alt-tab or cmd-tab macro depending on base layer
 
-add nav shortcuts between tabs?
-
+french layer:
+[ ] accented chars close to qwerty-fr
+    needs on MACOS "unicode hex input" as keyboard preference
+    remains to be tested on WIN
